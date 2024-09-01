63460562ff8b65b6ad8be97c692207288093fcd5
Ali-Hesham-1 <ali.hesham@egyis.com>
2024-09-01 15:34:02 +0300

    replacement container

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const leadCards = document.querySelectorAll(".lead-card")
+const cardsSection = document.getElementById("lead-cards-section")
+const dataSection = document.getElementById("data-section")
 const leadWhatsAppNumberLinks = document.querySelectorAll(".leadWhatsAppNumber")
 const leadAssignBtn = document.getElementById("lead-assign-btn")
+const backToCardsSection = document.getElementById("backToCardsSection-btn")
 
 leadCards.forEach(leadCard => {
     leadCard.addEventListener("click", function(e) {
@@ -8,7 +11,8 @@ leadCards.forEach(leadCard => {
         if (e.target.matches('.form-control') || e.target.closest('.form-control') || e.target.matches('.lead-assign-btn') || e.target.closest('.lead-assign-btn') ) {
             return; // Exit the function if the click was on the select list
         }
-        alert("lead-card is Clicked");
+        cardsSection.classList.add("d-none")
+        dataSection.classList.remove("d-none")
     });
 });
 
@@ -16,10 +20,17 @@ leadCards.forEach(leadCard => {
 leadWhatsAppNumberLinks.forEach(leadWhatsAppNumber =>{
     leadWhatsAppNumber.addEventListener("click" , function(e){
         alert("whatsApp Number Link is Clicked")
-        e.stopPropagation();
+        if (e.target.matches('.form-control') || e.target.closest('.form-control') || e.target.matches('.lead-assign-btn') || e.target.closest('.lead-assign-btn') ) {
+            return; // Exit the function if the click was on the select list
+        };
     })
 })
 
-leadAssignBtn.addEventListener("click" , function(e){
+leadAssignBtn?.addEventListener("click" , function(e){
     alert("Assigned is Confirmed")
+})
+
+backToCardsSection.addEventListener("click" , function(){
+    cardsSection.classList.remove("d-none")
+    dataSection.classList.add("d-none")
 })
