51c0d23ba9f267fed2b38fe5a052dc0e97824a05
Ali-Hesham-1 <ali.hesham@egyis.com>
2024-09-03 15:41:15 +0300

    update modal design for follow-up sheet

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const leadCards = document.querySelectorAll(".lead-card")
 const cardsSection = document.getElementById("lead-cards-section")
 const dataSection = document.getElementById("data-section")
 const leadWhatsAppNumberLinks = document.querySelectorAll(".leadWhatsAppNumber")
-const leadAssignBtn = document.getElementById("assignToButton")
+const leadAssignBtn = document.getElementById("lead-assign-btn")
 const backToCardsSection = document.getElementById("backToCardsSection-btn")
 
 leadCards.forEach(leadCard => {
     leadCard.addEventListener("click", function(e) {
         // Check if the click is on the select element or its descendants
-        if (e.target.matches('.form-control') || e.target.closest('.form-control') || e.target.matches('.assignToButton') || e.target.closest('.assignToButton') || e.target.matches('.whatsAppNumber') || e.target.closest('.whatsAppNumber') ) {
+        if (e.target.matches('.form-control') || e.target.closest('.form-control') || e.target.matches('.lead-assign-btn') || e.target.closest('.lead-assign-btn') ) {
             return; // Exit the function if the click was on the select list
         }
         cardsSection.classList.add("d-none")
@@ -20,7 +20,7 @@ leadCards.forEach(leadCard => {
 leadWhatsAppNumberLinks.forEach(leadWhatsAppNumber =>{
     leadWhatsAppNumber.addEventListener("click" , function(e){
         alert("whatsApp Number Link is Clicked")
-        if (e.target.matches('.form-control') || e.target.closest('.form-control') || e.target.matches('.assignToButton') || e.target.closest('.assignToButton') ) {            
+        if (e.target.matches('.form-control') || e.target.closest('.form-control') || e.target.matches('.lead-assign-btn') || e.target.closest('.lead-assign-btn') ) {
             return; // Exit the function if the click was on the select list
         };
     })
